d816d051ae4d0e00ac5f4e1a85bb2f3c44fe37e5
ahmad244 <34864567+ahmad244@users.noreply.github.com>
2024-03-10 14:41:23 +0300

    Add console.log statement for corsOptions

--- index.js
+++ index.js
@@ -35,4 +35,6 @@ app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
+  console.log("corsOptions ---->", corsOptions); 
+
 });
